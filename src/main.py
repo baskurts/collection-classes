4435492c800aba75b372a86b2fee807a206a9a78
baskurts <baskurts@chc.edu>
2023-11-10 10:43:35 -0500

    November 10

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from node.node import * 
+from stack.stack import *
 
 def main():
     # testInit()
@@ -10,7 +11,34 @@ def main():
     # testListSearch()
     # testListPosition()
     # testListCopy()
-    testListCopyWithTail()
+    # testListCopyWithTail()
+    testPush()
+
+def testPush():
+    print("Testing Push method in stack class")
+
+    s = stack()
+    print("Stack size is:", s.size()) # 0
+    print("Stack contains:", s)        # []
+
+    s.push = stack('S')
+    print(" Stack size is:", s.size()) # 1
+    print("Stack contains:", s)        # [S]
+
+    # s.push = stack('B')
+    s.push = stack(1)
+    print(" Stack size is:", s.size()) # 2
+    print("Stack contains:", s)        # [B S]
+
+    # s.push = stack('O')
+    s.push = stack((1,2))
+    print(" Stack size is:", s.size()) # 3
+    print("Stack contains:", s)        # [O B S]
+
+    s.push = stack('J')
+    s.push([1,2,3])
+    print(" Stack size is:", s.size()) # 4
+    print("Stack contains:", s)        # [J O B S]
 
 def testListCopyWithTail():
     print("Testing List Copy With Tail")
